cfac7af79c2ea9e4c534aa0ea7b74f79e4816b75
cabanzo <yesidcabanzo@gmail.com>
2023-06-18 17:26:38 -0500

    buscador de paises

--- Clase6/BuscadorDePaises.java
+++ Clase6/BuscadorDePaises.java
@@ -1,16 +1,16 @@
 package Clase6;
 import java.util.Scanner;
 
-public class BuscadorDePaises {
-    static void main(String[] args){
+class BuscadorPaisesGoogleMaps {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese un pais:  ");
+        System.out.print("Ingrese un país: ");
         String pais = scanner.nextLine();
 
         String url = "https://www.google.com/maps/search/" + pais;
 
         System.out.println("Enlace de búsqueda: " + url);
-
-
+        // Aquí podrías abrir la ubicación automáticamente utilizando la clase java.awt.Desktop
+        // o simplemente copiar y pegar el enlace en tu navegador web para abrirlo manualmente.
     }
 }
