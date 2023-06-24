3ee9f87146885a021a430cd61a871e9d86c0c420
cabanzo <yesidcabanzo@gmail.com>
2023-06-23 22:37:02 -0500

    Contador

---
+++ Clase6/Contador.java
@@ -0,0 +1,12 @@
+package Clase6;
+
+public class Contador {
+    public static void main(String[] args) {
+        int contador = 1;
+
+        while (contador <= 100){
+            System.out.println(contador);
+            contador++;
+        }
+    }
+}
